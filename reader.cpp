2de60e02bc5de750a81c4143d58407660da17c55
pengjiawei <917527280@qq.com>
2018-01-19 13:31:56 +0800

    add socket send and recv

--- reader.cpp
+++ reader.cpp
@@ -1,14 +1,115 @@
 #include <fstream>
 #include "hello.pb.h"
+#include <iostream>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+#include <unistd.h>
 using namespace std;
+const int BUFF_SIZE = 1024;
+char recvData[BUFF_SIZE];
+void createSocketAndReceive() {
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(12002);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    bzero(&(server_addr.sin_zero), 8);
+
+    int server_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sock_fd == -1) {
+        printf("Create goal socket failure!");
+    }
+    //绑定socket
+    int bind_result = bind(server_sock_fd, (struct sockaddr *) &server_addr,
+                           sizeof(server_addr));
+    if (bind_result == -1) {
+        printf("Set goal port failure!");
+    }
+    //listen
+    printf("i am listening!!!!!!!!!!\n");
+    if (listen(server_sock_fd, 3) == -1) {
+        printf("Listen goal port error!");
+    }
+
+    fd_set server_fd_set;
+    int max_fd = -1;
+    struct timeval tv;
+    int times = 0;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+//    while (true) {
+
+        FD_ZERO(&server_fd_set);
+        FD_SET(server_sock_fd, &server_fd_set);
+
+        int ret = select(server_sock_fd + 1, &server_fd_set, NULL, NULL, NULL);
+        printf("select return = %d\n", ret);
+        if (ret > 0) {
+            int client_sock_fd = 0;
+            bool flag = FD_ISSET(server_sock_fd, &server_fd_set);
+            printf("is in the set? = %d\n", flag);
+            if (flag) {
+                struct sockaddr_in client_address;
+                memset(&client_address, 0, sizeof(client_address));
+
+                socklen_t address_len = sizeof(client_address);
+                printf("i am accept!!!!!!!!!!\n");
+                client_sock_fd = accept(server_sock_fd,
+                                        (struct sockaddr *) &client_address, &address_len);
+
+                printf("client_sock_fd = %d\n", client_sock_fd);
+                if (client_sock_fd <= 0) {
+                    printf("client_sock_tf_in_controller < 0");
+                }
+            }
+
+            if (client_sock_fd != 0 && flag) {
+
+
+                memset(recvData, 0, BUFF_SIZE);
+
+                ssize_t byte_num = recv(client_sock_fd, recvData, sizeof(recvData),
+                                        0);
+
+                printf("recv_Ret = %d\n", byte_num);
+                if (byte_num > 0) {
+                    printf("%d\n", byte_num);
+                } else if (byte_num < 0) {
+                    close(client_sock_fd);
+                    client_sock_fd = 0;
+                } else {
+                    FD_CLR(client_sock_fd, &server_fd_set);
+                    client_sock_fd = 0;
+                }
+            }
+        } else if (ret == 0) {
+            FD_ZERO(&server_fd_set);
+            FD_SET(server_sock_fd, &server_fd_set);
+        }
+//    }
+}
 int main(){
+    createSocketAndReceive();
     const string saved_file = "./hello";
     hello::HelloWorld msg1;
     hello::HelloArray helloArray;;
     fstream input(saved_file.c_str(),ios::in | ios::binary);
-    if(!helloArray.ParseFromIstream(&input)){
+//    if(!helloArray.ParseFromIstream(&input)){
+//        cout << "Error Read"<<endl;
+//    }
+
+    if(!helloArray.ParseFromArray(recvData,BUFF_SIZE)){
         cout << "Error Read"<<endl;
     }
+
     printf("helloArray size = %d\n",helloArray.helloworld_size());
     for(int i = 0 ; i < helloArray.helloworld_size();++i){
         msg1 = helloArray.helloworld(i);
--- writer.cpp
+++ writer.cpp
@@ -3,8 +3,63 @@
 //
 #include <fstream>
 #include "hello.pb.h"
+#include <iostream>
+#include<string.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include <netinet/in.h>
+#include<arpa/inet.h>
+#include <unistd.h>
+
+const int BUFF_SIZE = 1024;
 using namespace std;
+char buff[BUFF_SIZE];
+void createSocketAndSend(const std::string& ip,const std::string& port){
+    int sock_id;
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(port.c_str()));
+
+    inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr);
+
+    //connect socket
+    if ((sock_id = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+
+        printf("Create socket failed %d\n", sock_id);
+
+    }
+
+    int reuseflag = 1;
+    setsockopt(sock_id, SOL_SOCKET, SO_REUSEADDR, (const char*) &reuseflag,
+               sizeof(int)); //set reused
+
+    struct linger clsflag;
+    clsflag.l_onoff = 1;
+    clsflag.l_linger = 2000;
+    setsockopt(sock_id, SOL_SOCKET, SO_LINGER, (const struct linger*) &clsflag,
+               sizeof(struct linger)); //set closed
+
+    int i_ret = connect(sock_id, (struct sockaddr *) &serv_addr,
+                        sizeof(serv_addr));
+
+    printf("finish connect\n");
+    if (i_ret == -1) {
+        printf("Connect socket failed\n");
+        close(sock_id);
+    }
+
+    send(sock_id, buff, sizeof(buff), 0);
+
+    close(sock_id);
+}
+
 int main(){
+    const string ip = "192.168.31.198";
+    const string port = "12002";
     const string saved_file = "./hello";
     hello::HelloWorld* msg1;
     hello::HelloArray helloArray;
@@ -17,10 +72,27 @@ int main(){
     msg1->set_id(02);
     msg1->set_str("hello_world2");
 
+    msg1 = helloArray.add_helloworld();
+    msg1->set_id(03);
+    msg1->set_str("hello_world3");
+
     printf("size = %d\n",helloArray.helloworld_size());
+    printf("len = %d\n",helloArray.ByteSize());
     fstream output(saved_file.c_str(),ios::out | ios::trunc | ios::binary);
-    if(!helloArray.SerializeToOstream(&output)){
+
+    memset(buff, 0, BUFF_SIZE);
+
+//    if(!helloArray.SerializeToOstream(&output)){
+//        cout << "Error" <<endl;
+//    }
+    if(!helloArray.SerializeToArray(buff,BUFF_SIZE)){
         cout << "Error" <<endl;
     }
+
+    createSocketAndSend(ip,port);
+    if(!helloArray.ParseFromArray(buff,BUFF_SIZE)){
+        cout << "Error Read"<<endl;
+    }
+    printf("helloArray size = %d\n",helloArray.helloworld_size());
     return 0;
 }
